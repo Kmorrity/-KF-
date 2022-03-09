b74a4ffb7821b7bc1677b687424f957050dd97d7
Kmorrity <68260832+Kmorrity@users.noreply.github.com>
2022-03-09 14:48:57 +0800

    Update EKF.m

--- 卡尔曼滤波/EKF.m
+++ 卡尔曼滤波/EKF.m
@@ -1,21 +1,21 @@
 close all;
 clear all;
 
-mul=0; % ¾ùÖµ
-S1=0.1; % Ğ­·½²î
-data1=mvnrnd(mul, S1, 100); % ²úÉú¸ßË¹·Ö²¼Êı¾İ
+mul=0; % å‡å€¼
+S1=0.1; % åæ–¹å·®
+data1=mvnrnd(mul, S1, 100); % äº§ç”Ÿé«˜æ–¯åˆ†å¸ƒæ•°æ®
 
 plot(data1,'b+');
 hold on;
-%%  ÕæÊµ¹ì¼£Ä£Äâ
-kx = .01;   ky = .05;       % ×èÄáÏµÊı
-g = 9.8;                    % ÖØÁ¦
-t = 15;                     % ·ÂÕæÊ±¼ä
-Ts = 0.1;                   % ²ÉÑùÖÜÆÚ 
-len = fix(t/Ts);            % ·ÂÕæ²½Êı 
-dax = 3; day = 3;       % ÏµÍ³ÔëÉù
+%%  çœŸå®è½¨è¿¹æ¨¡æ‹Ÿ
+kx = .01;   ky = .05;       % é˜»å°¼ç³»æ•°
+g = 9.8;                    % é‡åŠ›
+t = 15;                     % ä»¿çœŸæ—¶é—´
+Ts = 0.1;                   % é‡‡æ ·å‘¨æœŸ 
+len = fix(t/Ts);            % ä»¿çœŸæ­¥æ•° 
+dax = 3; day = 3;       % ç³»ç»Ÿå™ªå£°
 X = zeros(len,4); 
-X(1,:) = [0, 50, 500, 0]; % ×´Ì¬Ä£ÄâµÄ³õÖµ
+X(1,:) = [0, 50, 500, 0]; % çŠ¶æ€æ¨¡æ‹Ÿçš„åˆå€¼
 for k=2:len
     x = X(k-1,1); vx = X(k-1,2); y = X(k-1,3); vy = X(k-1,4); 
     x = x + vx*Ts;
@@ -24,14 +24,14 @@ for k=2:len
     vy = vy + (ky*vy^2-g+day*randn(1))*Ts;
     X(k,:) = [x, vx, y, vy];
 end
-%%  ¹¹ÔìÁ¿²âÁ¿
-dr = 8;  dafa = 0.1;        % Á¿²âÔëÉù
+%%  æ„é€ é‡æµ‹é‡
+dr = 8;  dafa = 0.1;        % é‡æµ‹å™ªå£°
 for k=1:len
     r = sqrt(X(k,1)^2+X(k,3)^2) + dr*randn(1,1);
     a = atan(X(k,1)/X(k,3))*57.3 + dafa*randn(1,1);
     Z(k,:) = [r, a];
 end
-%% ekf ÂË²¨
+%% ekf æ»¤æ³¢
 Qk = diag([0; dax/10; 0; day/10])^2;
 Rk = diag([dr; dafa])^2;
 Pk = 10*eye(4);
@@ -41,17 +41,17 @@ X_est = zeros(len,4);
 x_forecast = zeros(4,1);
 z = zeros(4,1);
 for k=1:len
-    % 1 ×´Ì¬Ô¤²â    
+    % 1 çŠ¶æ€é¢„æµ‹    
     x1 = x_hat(1) + x_hat(2)*Ts;
     vx1 = x_hat(2) + (-kx*x_hat(2)^2)*Ts;
     y1 = x_hat(3) + x_hat(4)*Ts;
     vy1 = x_hat(4) + (ky*x_hat(4)^2-g)*Ts;
-    x_forecast = [x1; vx1; y1; vy1];        %Ô¤²âÖµ
-    % 2  ¹Û²âÔ¤²â
+    x_forecast = [x1; vx1; y1; vy1];        %é¢„æµ‹å€¼
+    % 2  è§‚æµ‹é¢„æµ‹
     r = sqrt(x1*x1+y1*y1);
     alpha = atan(x1/y1)*57.3;
     y_yuce = [r,alpha]';
-    %  ×´Ì¬¾ØÕó
+    %  çŠ¶æ€çŸ©é˜µ
     vx = x_forecast(2);  vy = x_forecast(4);
     F = zeros(4,4);
     F(1,1) = 1;  F(1,2) = Ts;
@@ -59,15 +59,15 @@ for k=1:len
     F(3,3) = 1;  F(3,4) = Ts;
     F(4,4) = 1+2*ky*vy*Ts;
     Pkk_1 = F*Pk*F'+Qk;
-    % ¹Û²â¾ØÕó
+    % è§‚æµ‹çŸ©é˜µ
     x = x_forecast(1); y = x_forecast(3);
     H = zeros(2,4);
     r = sqrt(x^2+y^2);  xy2 = 1+(x/y)^2;
     H(1,1) = x/r;  H(1,3) = y/r;
     H(2,1) = (1/y)/xy2;  H(2,3) = (-x/y^2)/xy2;
     
-    Kk = Pkk_1*H'*(H*Pkk_1*H'+Rk)^-1;       %¼ÆËãÔöÒæ
-    x_hat = x_forecast+Kk*(Z(k,:)'-y_yuce);      %Ğ£Õı
+    Kk = Pkk_1*H'*(H*Pkk_1*H'+Rk)^-1;       %è®¡ç®—å¢ç›Š
+    x_hat = x_forecast+Kk*(Z(k,:)'-y_yuce);      %æ ¡æ­£
     Pk = (eye(4)-Kk*H)*Pkk_1;
     X_est(k,:) = x_hat;
 end
@@ -81,3 +81,4 @@ ylabel('Y');
 title('EKF simulation');
 legend('real', 'measurement', 'ekf estimated');
 axis([-5,230,290,530]);
+
